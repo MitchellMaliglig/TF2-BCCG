3c3aeac5728c50d8d9b6029c72c21c77104a5617
MitchellMaliglig <mitchell.maliglig@gmail.com>
2024-09-28 00:58:37 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,31 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "username" text UNIQUE NOT NULL,
+  "hashedPassword" text NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT 'now()',
+  "updatedAt" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "entries" (
+  "entryId" serial PRIMARY KEY,
+  "userId" int,
+  "title" text NOT NULL,
+  "description" text NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT 'now()',
+  "updatedAt" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+CREATE TABLE "commands" (
+  "commandId" serial PRIMARY KEY,
+  "entryId" int,
+  "command" text NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "commands" ADD FOREIGN KEY ("entryId") REFERENCES "entries" ("entryId");
